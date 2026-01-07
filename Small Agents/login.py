727e6c4e2cc6e5823d1ecdf0b64285fac0b71538
Ruchira Madhushan Rajapaksha <52396694+LordMaduz@users.noreply.github.com>
2026-01-07 20:42:15 +0800

    Agentic AI POC using small agents framework

---
+++ Small Agents/login.py
@@ -0,0 +1,25 @@
+
+"""
+
+---------------------------------
+Setup & Run Instructions
+---------------------------------
+
+1. Create a virtual environment:
+   python3 -m venv venv
+
+2. Activate the virtual environment:
+   source venv/bin/activate
+
+3. Install dependencies:
+   pip install smolagents -U
+
+4. Run the script:
+   python3 tool_test.py
+   HF Token: ADD YOUR HF TOKEN 
+"""
+
+from huggingface_hub import login
+
+login()
+
